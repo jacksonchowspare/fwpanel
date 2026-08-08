0425ab92d4164c39c56ae5358a3993a3ac3ab82a
jacksonchowspare <jacksonchowspare@users.noreply.github.com>
2026-08-08 22:54:23 +0800

    v1.7.3: two-column responsive layout, wider container (1280px)

--- install.sh
+++ install.sh
@@ -20,7 +20,7 @@ set -Eeuo pipefail
 
 # ------------------------------ 常量 ------------------------------
 readonly SCRIPT_NAME="fwpanel 防火墙面板安装包"
-readonly SCRIPT_VERSION="1.7.2"
+readonly SCRIPT_VERSION="1.7.3"
 readonly LOG_FILE="/var/log/fwpanel-install.log"
 readonly APP_DIR="/usr/local/lib/fwpanel"
 readonly ETC_DIR="/etc/fwpanel"
